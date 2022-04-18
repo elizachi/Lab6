d6fabf87cbb8984a3d3efba79e5cbd7cb57acb1c
Liza <Lizachig2003@gmail.com>
2022-04-18 14:07:17 +0300

    Добавлены классы Response и Request.

---
+++ common/src/main/java/ru/itmo/common/requests/Request.java
@@ -0,0 +1,31 @@
+package ru.itmo.common.requests;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class Request {
+    public final String commandName;
+    public final Object argument;
+
+    public Request(String commandName, Object argument) {
+        this.commandName = commandName;
+        this.argument = new Gson().toJson(argument);
+    }
+
+    public static Request fromJson(String json) {
+        return new Gson().fromJson(json, Request.class);
+    }
+
+
+    public <T> T getArgumentAs(Class<T> clazz) {
+        return new Gson().fromJson((String) argument, clazz);
+    }
+
+    public <T> Object getArgumentAs(TypeToken<T> typeToken) {
+        return new Gson().fromJson((String) argument, typeToken.getType());
+    }
+
+    public String toJson() {
+        return new Gson().toJson(this);
+    }
+}
