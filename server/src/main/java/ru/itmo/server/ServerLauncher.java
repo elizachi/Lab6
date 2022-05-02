2877f4fa5d8cfcceb7b3ea78a29888a599e6d426
Liza <Lizachig2003@gmail.com>
2022-05-02 15:50:09 +0300

    Всё работает, однако сервер ломатеся при попытке отправить супер огромное количество информации (for example 10000 строчек show)

--- server/src/main/java/ru/itmo/server/ServerLauncher.java
+++ server/src/main/java/ru/itmo/server/ServerLauncher.java
@@ -1,9 +1,12 @@
 package ru.itmo.server;
 
-public class ServerLauncher {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class ServerLauncher {
+    public static final Logger log = LogManager.getLogger(ru.itmo.server.ServerLauncher.class.getName());
     public static void main(String[] args) {
-        Server server = new Server();
-        server.serverStart();
+        Server server = new Server("localhost", 65100);
+        server.start();
     }
 }
